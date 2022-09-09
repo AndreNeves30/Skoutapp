5bd94e2d588d6111de0c060138a4ad79490fa50c
AndreNeves30 <andre.neves30@etec.sp.gov.br>
2022-09-08 22:14:37 -0300

    mo sono

--- app/src/main/java/br/com/etecia/skoutapp/MainActivity.java
+++ app/src/main/java/br/com/etecia/skoutapp/MainActivity.java
@@ -2,6 +2,7 @@ package br.com.etecia.skoutapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -42,6 +43,10 @@ public class MainActivity extends AppCompatActivity {
 
                 //validando usuario e senha
                 if ( usuario.equals("etecia") && senha.equals("etecia")){
+                    //abrir janela de menu
+                    startActivity(new Intent(getApplicationContext(),
+                            MenuPrincipalActivity.class));
+                    finish();
 
                 } else {
                     Toast.makeText(getApplicationContext(),
